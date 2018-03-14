cfb6a14400ab63091e7d320e086625b744d55bb6
CrumpLab <crumpmj@gmail.com>
2018-03-14 08:19:04 -0400

    added run.r

---
+++ run.R
@@ -0,0 +1,19 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+port <- Sys.getenv('PORT')
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
+
