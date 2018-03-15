7cd46431d80df301489949822167e6c8170e704f
CrumpLab <crumpmj@gmail.com>
2018-03-15 07:12:09 -0400

    add plotly library

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("hypothesisr")
+my_packages = c("hypothesisr","plotly")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
